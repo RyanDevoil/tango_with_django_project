48a4a8547f00f6a226dcc6c158ed88d2fb675350
Ryan Devoil <2257271D@Student.gla.ac.uk>
2018-02-09 16:20:15 +0000

    Added login functionality, and locked some functions behind logging in.

--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse
 from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 
@@ -31,6 +34,7 @@ def show_category(request, category_name_slug):
         context_dict['category']= None
     return render(request, 'rango/category.html', context_dict)
 
+@login_required
 def add_category(request):
     form = CategoryForm()
     if request.method == 'POST':
@@ -42,6 +46,7 @@ def add_category(request):
             print(form.errors)
     return render(request, 'rango/add_category.html', {'form':form})
 
+@login_required
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
@@ -86,3 +91,29 @@ def register(request):
     return render(request, 'rango/register.html', {'user_form':user_form,
                                                        'profile_form':profile_form,
                                                        'registered':registered})
+
+def user_login(request):
+    if request.method == 'POST':
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+            else:
+                return render(request, 'rango/login.html', {'errormessage':'Your account has been disabled.'})
+        else:
+            print("Invalid login details: {0}, {1}".format(username, password))
+            return render(request, 'rango/login.html', {'errormessage': 'Invalid username or password.'})
+    else:
+        return render(request, 'rango/login.html', {})
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
+
+@login_required
+def restricted(request):
+    return render(request, 'rango/restricted.html')
